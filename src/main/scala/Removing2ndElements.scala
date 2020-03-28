15bc82f2f4f455c178c902cb419051040a82e01b
Deepak <j.thepac@gmail.com>
2020-03-28 08:20:37 -0400

    Scala Snippets1

---
+++ src/main/scala/Removing2ndElements.scala
@@ -0,0 +1,13 @@
+import scala.collection.mutable._
+
+object Removing2ndElements {
+  def main(args: Array[String]): Unit = {
+    println(Kata.removeEveryOther(List("abc", "sdfsdf","f",1,2)))
+  }
+}
+
+
+object Kata {
+  def removeEveryOther[T](list: List[T]): List[T] = {
+    list.indices.collect{case i if i%2==0 => list(i)}.toList
+  }}
