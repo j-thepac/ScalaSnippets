23ae1e9d4162cd8f3a257a8401ae6b675f3a30c7
Deepak Jayaprakash <j.thepac@gmail.com>
2022-05-29 07:04:44 +0000

    Regexpin

---
+++ src/main/scala/ScalaSnippets/RegexPin.scala
@@ -0,0 +1,13 @@
+import scala.util.matching.Regex
+
+object RegexPin {
+    def main(args:Array[String]):Unit={
+        val exp:Regex="^\\d{4}$|^||d{6}$".r
+        val fn:String=>Boolean = (s:String) => s.findAllinMatchIn(s).length >0
+        print(fn("1234"))
+        print(fn("12345"))
+        print(fn("123456"))
+    }
+
+
+}
